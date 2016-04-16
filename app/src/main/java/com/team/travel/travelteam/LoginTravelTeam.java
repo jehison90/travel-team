232af64a570818ed3a577c48de44128d9e73776f
Jehison Prada <jpradau@psl.com.co>
2016-04-16 13:07:40 -0500

    Change background image added

--- app/src/main/java/com/team/travel/travelteam/LoginTravelTeam.java
+++ app/src/main/java/com/team/travel/travelteam/LoginTravelTeam.java
@@ -71,16 +71,7 @@ public class LoginTravelTeam extends AppCompatActivity implements LoaderCallback
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
+
 
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
